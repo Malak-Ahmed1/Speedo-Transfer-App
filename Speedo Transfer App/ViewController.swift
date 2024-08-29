5b5fc948e05fefcd85a0aa1dab24cd01ef699f0d
Malak Ahmed <malakahmed55565@gmail.com>
2024-08-29 09:44:30 +0300

    Initial Commit

---
+++ Speedo Transfer App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Speedo Transfer App
+//
+//  Created by 1234 on 29/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
